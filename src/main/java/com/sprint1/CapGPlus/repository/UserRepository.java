94e97a47bdd3d2dbc0ff7803f9f83b22148582ad
Snehal Mane <snehal-shahaji.mane@capgemini.com>
2022-09-26 23:21:24 +0530

    Added repository, service, service impl and controller templates

--- src/main/java/com/sprint1/CapGPlus/repository/UserRepository.java
+++ src/main/java/com/sprint1/CapGPlus/repository/UserRepository.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.sprint1.CapGPlus.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	@Query("SELECT e FROM User e WHERE e.firstName = :name")
 	public List<User> findByFirstName(@Param("name") String firstName);
