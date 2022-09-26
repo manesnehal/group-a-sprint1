ef74664e5bf2d17768dfe0d5f35e416356bd6644
Snehal Mane <snehal-shahaji.mane@capgemini.com>
2022-09-26 23:24:13 +0530

    Changed to PostRepository in PostServiceImpl

--- src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
+++ src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
@@ -3,8 +3,10 @@ package com.sprint1.CapGPlus.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sprint1.CapGPlus.repository.PostRepository;
+
 @Service
 public class PostServiceImpl implements PostService {
 	@Autowired
-	private PostService postService;
+	private PostRepository postRepository;
 }
