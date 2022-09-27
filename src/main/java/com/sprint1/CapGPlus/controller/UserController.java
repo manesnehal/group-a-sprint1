0043abebef4471ba2f394c5059bc590c5455fe07
shardul1501 <shardulshroff936@gmail.com>
2022-09-27 16:14:05 +0530

    bug fixes

--- src/main/java/com/sprint1/CapGPlus/controller/UserController.java
+++ src/main/java/com/sprint1/CapGPlus/controller/UserController.java
@@ -1,66 +1,14 @@
 package com.sprint1.CapGPlus.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sprint1.CapGPlus.entity.User;
 import com.sprint1.CapGPlus.service.UserService;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserService userServ;
-
-	@GetMapping("/user/{firstName}")
-	private ResponseEntity<List<User>> getUsersByFirstName(@PathVariable String firstName) {
-		return new ResponseEntity<>(userServ.findByFirstName(firstName), HttpStatus.OK);
-	}
-
-	@GetMapping("/users/{id}")
-	private ResponseEntity<Object> getEmployeeById(@PathVariable int id) {
-		Optional<User> userData = userServ.getUserById(id);
-		if (userData.isPresent())
-			return new ResponseEntity<>(userData, HttpStatus.OK);
-		else
-			return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-	}
-
-	@GetMapping("/users")
-	private ResponseEntity<List<User>> getUsers() {
-		return new ResponseEntity<>(userServ.getUsers(), HttpStatus.OK);
-	}
-
-	@PostMapping("/user")
-	private ResponseEntity<String> saveUser(@RequestBody User emp) {
-		userServ.saveUser(emp);
-		return new ResponseEntity<>("User created", HttpStatus.CREATED);
-	}
-
-	@DeleteMapping("/users/{id}")
-	private ResponseEntity<String> deleteuserById(@PathVariable int id) {
-		if (userServ.deleteUserById(id))
-			return new ResponseEntity<>("User deleted!", HttpStatus.NO_CONTENT);
-		return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-	}
-
-	@PutMapping("/users/{id}")
-	private ResponseEntity<Object> updateUserById(@RequestBody User usr, @PathVariable int id) {
-		User updatedUser = userServ.updateUserById(id, usr);
-		if (updatedUser == null)
-			return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
-		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-	}
+	private UserService userService;
 
 }
--- src/main/java/com/sprint1/CapGPlus/service/UserServiceImpl.java
+++ src/main/java/com/sprint1/CapGPlus/service/UserServiceImpl.java
@@ -1,56 +1,13 @@
 package com.sprint1.CapGPlus.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.sprint1.CapGPlus.entity.User;
 import com.sprint1.CapGPlus.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
-	@Override
-	public User saveUser(User user) {
-		return userRepo.save(user);
-	}
-	
-	@Override
-	public List<User> getUsers() {
-		return userRepo.findAll();
-	}
-
-	@Override
-	public Optional<User> getUserById(int id) {
-		return userRepo.findById(id);
-	}
-
-	@Override
-	public boolean deleteUserById(int id) {
-		// Check if User exists
-		if (!userRepo.existsById(id))
-			return false;
-
-		userRepo.deleteById(id);
-		return true;
-	}
-
-	@Override
-	public User updateUserById(int id, User user) {
-		Optional<User> UserOptional = userRepo.findById(id);
-		if (UserOptional.isEmpty())
-			return null;
-		user.setId(id);
-		return userRepo.save(user);
-	}
-
-	@Override
-	public List<User> findByFirstName(String firstName) {
-		return userRepo.findByFirstName(firstName);
-	}
 }
