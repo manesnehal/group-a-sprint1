d03b0ddad388894a1953a22e5d2a911e69857149
shardul1501 <shardulshroff936@gmail.com>
2022-09-27 16:04:22 +0530

    exceptions added

---
+++ src/main/java/com/sprint1/CapGPlus/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.sprint1.CapGPlus.exception;
+
+public class InvalidCredentialsException extends Exception {
+	public InvalidCredentialsException() {
+		super();
+	}
+
+	public InvalidCredentialsException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/main/java/com/sprint1/CapGPlus/exception/PasswordMatchException.java
@@ -0,0 +1,11 @@
+package com.sprint1.CapGPlus.exception;
+
+public class PasswordMatchException extends Exception {
+	public PasswordMatchException() {
+		super();
+	}
+
+	public PasswordMatchException(String msg) {
+		super(msg);
+	}
+}
