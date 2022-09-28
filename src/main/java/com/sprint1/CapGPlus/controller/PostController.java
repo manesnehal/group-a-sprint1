f184987ad3e047d7b042d6a7b9df318fcb635ccc
Snehal Mane <snehal-shahaji.mane@capgemini.com>
2022-09-28 17:38:23 +0530

    Minor formatting changes

--- src/main/java/com/sprint1/CapGPlus/controller/PostController.java
+++ src/main/java/com/sprint1/CapGPlus/controller/PostController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sprint1.CapGPlus.entity.Post;
@@ -15,12 +13,10 @@ import com.sprint1.CapGPlus.service.PostService;
 
 @RestController
 public class PostController {
-	
+
 	@Autowired
 	private PostService postService;
-	
-//	@GetMapping("/users")
-	//@RequestMapping(name="/post", method = RequestMethod.GET)
+
 	@GetMapping("/post")
 	public ResponseEntity<List<Post>> getAllPosts() {
 		List<Post> list = postService.getAllPosts();
