94786d8bf28740fefb5ad6ec4dd18ea71291e59c
aydubey <aydubey@INPRN8520010006.corp.capgemini.com>
2022-09-28 17:24:17 +0530

    post controller :-getAllPosts

--- src/main/java/com/sprint1/CapGPlus/service/PostService.java
+++ src/main/java/com/sprint1/CapGPlus/service/PostService.java
@@ -1,8 +1,11 @@
 package com.sprint1.CapGPlus.service;
 
+import java.util.List;
+
 import com.sprint1.CapGPlus.entity.Post;
 
 public interface PostService {
 
-
+	public List<Post> getAllPosts();
+	
 }
--- src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
+++ src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
@@ -1,18 +1,21 @@
 package com.sprint1.CapGPlus.service;
 
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sprint1.CapGPlus.entity.Post;
-import com.sprint1.CapGPlus.entity.User;
 import com.sprint1.CapGPlus.repository.PostRepository;
-import com.sprint1.CapGPlus.repository.UserRepository;
 
 @Service
 public class PostServiceImpl implements PostService {
 	@Autowired
 	private PostRepository postRepository;
+	
+	@Override
+	public List<Post> getAllPosts() {
+		return postRepository.findAll();
+	}
 
 }
