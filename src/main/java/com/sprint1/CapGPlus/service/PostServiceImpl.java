1286d04dc89e1cb247230ac6c4c3a37159b7ff6a
shardul1501 <shardulshroff936@gmail.com>
2022-09-29 11:22:43 +0530

    Minor change

--- src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
+++ src/main/java/com/sprint1/CapGPlus/service/PostServiceImpl.java
@@ -29,11 +29,10 @@ public class PostServiceImpl implements PostService {
 		return postRepository.findAll().stream().map(postDTOService::convertToOuterDTO).collect(Collectors.toList());
 	}
 
-	
 	@Override
 	public PostDTOOuter getPostById(int postId) throws PostNotFoundException {
 		if (!postRepository.existsById(postId))
 			throw new PostNotFoundException();
-		return postDTOService.convertToDTO(postRepository.findById(postId).get());
+		return postDTOService.convertToOuterDTO(postRepository.findById(postId).get());
 	}
 }
