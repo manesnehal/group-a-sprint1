488fd34185bd0d60936c51702ae94fe9cbb4f6be
Snehal Mane <snehal-shahaji.mane@capgemini.com>
2022-09-29 15:22:45 +0530

    User custom query working; returning empty array when no comments instead of message

--- src/main/java/com/sprint1/CapGPlus/repository/UserRepository.java
+++ src/main/java/com/sprint1/CapGPlus/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.sprint1.CapGPlus.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +9,7 @@ import com.sprint1.CapGPlus.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-	@Query(value = "SELECT e FROM User e WHERE e.firstName = :name", nativeQuery = true)
-	public List<User> findByFirstName(@Param("name") String firstName);
-
-	@Query(value = "SELECT e FROM User e WHERE e.userName = :name", nativeQuery = true)
+	@Query(value = "SELECT e FROM User e WHERE e.userName = :name")
 	public User findByUserName(@Param("name") String userName);
 
 }
--- src/main/java/com/sprint1/CapGPlus/service/CommentServiceImpl.java
+++ src/main/java/com/sprint1/CapGPlus/service/CommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sprint1.CapGPlus.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,20 +24,21 @@ public class CommentServiceImpl implements CommentService {
 
 	@Autowired
 	private CommentDTOService commentDTOService;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
-	public List<CommentDTO> getAllCommentsByUser(int userId) throws UserNotFoundException, CommentDoesNotExistException {
-		if(!userRepository.existsById(userId))
+	public List<CommentDTO> getAllCommentsByUser(int userId)
+			throws UserNotFoundException, CommentDoesNotExistException {
+		if (!userRepository.existsById(userId))
 			throw new UserNotFoundException();
 		List<Comment> list = commentRepository.getAllCommentsByUser(userId);
-		if(list.isEmpty())
-			throw new CommentDoesNotExistException();
+		if (list.isEmpty())
+			return new ArrayList<CommentDTO>();
 		return commentRepository.getAllCommentsByUser(userId).stream().map(commentDTOService::convertToDTO)
 				.collect(Collectors.toList());
 	}
@@ -44,11 +46,11 @@ public class CommentServiceImpl implements CommentService {
 	@Override
 	public List<CommentDTO> getAllCommentsOnAPost(int postId)
 			throws CommentDoesNotExistException, PostNotFoundException {
-		if(!postRepository.existsById(postId))
+		if (!postRepository.existsById(postId))
 			throw new PostNotFoundException();
 		List<Comment> list = commentRepository.getAllCommentsOnAPost(postId);
-		if(list.isEmpty())
-			throw new CommentDoesNotExistException();
+		if (list.isEmpty())
+			return new ArrayList<CommentDTO>();
 		return commentRepository.getAllCommentsOnAPost(postId).stream().map(commentDTOService::convertToDTO)
 				.collect(Collectors.toList());
 	}
